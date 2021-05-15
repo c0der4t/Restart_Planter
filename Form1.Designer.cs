170326e9d7f8bf0368d9092ccb11236a24bbcf51
Monte Ekron <monte@ekronds.co.za>
2021-05-15 19:07:38 +0200

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace RestartPlanter
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.imgIcon = new System.Windows.Forms.PictureBox();
+            this.lblOpt = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imgIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgIcon
+            // 
+            this.imgIcon.Image = global::RestartPlanter.Properties.Resources.AdminTool;
+            this.imgIcon.Location = new System.Drawing.Point(12, 12);
+            this.imgIcon.Name = "imgIcon";
+            this.imgIcon.Size = new System.Drawing.Size(132, 107);
+            this.imgIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgIcon.TabIndex = 1;
+            this.imgIcon.TabStop = false;
+            // 
+            // lblOpt
+            // 
+            this.lblOpt.AutoSize = true;
+            this.lblOpt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOpt.Location = new System.Drawing.Point(150, 60);
+            this.lblOpt.Name = "lblOpt";
+            this.lblOpt.Size = new System.Drawing.Size(344, 20);
+            this.lblOpt.TabIndex = 2;
+            this.lblOpt.Text = "An Error Occured. Please Try Again Later.";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 129);
+            this.Controls.Add(this.lblOpt);
+            this.Controls.Add(this.imgIcon);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Optimizing System";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgIcon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox imgIcon;
+        private System.Windows.Forms.Label lblOpt;
+    }
+}
+
